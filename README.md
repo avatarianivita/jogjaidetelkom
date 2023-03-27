12ce4c7e4f80c31c795db328b6b7dfc68f5877b9
avatarianivita <129029126+avatarianivita@users.noreply.github.com>
2023-03-27 14:45:41 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# jogjaidetelkom
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-ah9slg)
---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
+    <HelloWorld msg="Welcome to Jogjaide" />
+    <tombol msg="Welcome to Jogjaide" class="btn btn-danger" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import tombol from './components/tombol.vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    tombol,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
