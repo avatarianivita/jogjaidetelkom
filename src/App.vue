a7fdb39c8d6e7f89df29cabea6e7ac5a05a0fa00
avatarianivita <129029126+avatarianivita@users.noreply.github.com>
2023-03-27 14:48:55 +0800

    name modified

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
-    <HelloWorld msg="Welcome to Jogjaide" />
+    <HelloWorld msg="Welcome to Jogjaide Makassar" />
     <tombol msg="Welcome to Jogjaide" class="btn btn-danger" />
   </div>
 </template>
